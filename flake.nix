edf854c5caa1e6ccbbe34d3a4c50e69ef6adcb6b
Oxy8 <34687508+Oxy8@users.noreply.github.com>
2025-12-24 23:17:53 -0300

    lite-xl

--- flake.nix
+++ flake.nix
@@ -2,13 +2,15 @@
   description = "Config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix-software-center.url = "github:vlinkz/nix-software-center";
+    
+    nix-xl.url = "github:passivelemon/nix-xl";
   };
 
   outputs = { nixpkgs, nix-software-center, home-manager, ... }@inputs:
@@ -31,10 +33,28 @@
 		      	modules = [
 		      		./V15/configuration.nix
 					./V15/keyboard-led-update.nix
+					
+					home-manager.nixosModules.home-manager
+					{
+						home-manager.useGlobalPkgs = true;
+						home-manager.useUserPackages = true;
+						
+						home-manager.users.estevan = { pkgs, ... }: {
+							imports = [
+								inputs.nix-xl.homeModules.nix-xl
+							];
+							
+							home.stateVersion = "25.11"; 							  
+							
+							programs.lite-xl = {
+								enable = true;
+								fonts.font = "FiraCodeNerdFontMono-Retina";
+								fonts.codeFont = "FiraCodeNerdFontMono-Retina";
+							};							
+						};
+					}
 		      	];
-		      	
 			};
-				
 		};
 		
 		nixosModules = {
