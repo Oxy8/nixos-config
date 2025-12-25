a493388a2765f5c834201ae11e5f9b58641a6ba2
Oxy8 <34687508+Oxy8@users.noreply.github.com>
2025-12-24 23:43:11 -0300

    Remove home-manager/lite-xl

--- flake.nix
+++ flake.nix
@@ -3,14 +3,9 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
     nix-software-center.url = "github:vlinkz/nix-software-center";
-    
-    nix-xl.url = "github:passivelemon/nix-xl";
+
   };
 
   outputs = { nixpkgs, nix-software-center, home-manager, ... }@inputs:
@@ -34,25 +29,6 @@
 		      		./V15/configuration.nix
 					./V15/keyboard-led-update.nix
 					
-					home-manager.nixosModules.home-manager
-					{
-						home-manager.useGlobalPkgs = true;
-						home-manager.useUserPackages = true;
-						
-						home-manager.users.estevan = { pkgs, ... }: {
-							imports = [
-								inputs.nix-xl.homeModules.nix-xl
-							];
-							
-							home.stateVersion = "25.11"; 							  
-							
-							programs.lite-xl = {
-								enable = true;
-								fonts.font = "FiraCodeNerdFontMono-Retina";
-								fonts.codeFont = "FiraCodeNerdFontMono-Retina";
-							};							
-						};
-					}
 		      	];
 			};
 
